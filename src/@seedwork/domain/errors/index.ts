3407493fa8bed3210066e13ae22fa63ecb3903c4
celsopires1999 <celso.pires1999@gmail.com>
2022-06-18 02:46:40 +0000

    feat: Organizando exports com indexes

---
+++ src/@seedwork/domain/errors/index.ts
@@ -0,0 +1,6 @@
+// created from 'create-ts-index'
+
+export * from './exists.error';
+export * from './invalid-uuid.error';
+export * from './not-found.error';
+export * from './validation-error';
---
+++ src/category/application/index.ts
@@ -0,0 +1,4 @@
+// created from 'create-ts-index'
+
+export * from './errors';
+export * from './use-cases';
